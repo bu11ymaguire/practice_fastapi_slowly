1716dcd3e70a7fc0ab929203322f9bc1c93e366f
jwkim <jwkim@hanyang.ac.kr>
2026-01-11 15:26:07 +0900

    parameter pratice

--- first.py
+++ first.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(str, Enum):
+    alxenet = "alxenet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"meassage":"Hello World"}
+
+'''
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+순서 문제 주의하기!
+'''
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alxenet:
+        return {"model_name": model_name,"message": "Deep Learning FTW!"}
+
+    if mode_name.value == "lenet":
+        return {"model_name": model_name, "message":"LeCNN all the images"}
+
+    return {"model_name": model_name, "message":"Have some residuals"}        
